bc9dfb104c721006ee032875b691efd67594ba9c
Jean Fonseca <jeanzoldyck07@gmail.com>
2024-11-27 21:50:46 -0300

    1 Commit

---
+++ AnalisadorLexxico/src/br/com/jean/compiler/main/AnalisadorLexxico.java
@@ -0,0 +1,31 @@
+package br.com.jean.compiler.main;
+
+import br.com.jean.compiler.exceptions.LexicalException;
+import br.com.jean.compiler.lexico.IsiScanner;
+import br.com.jean.compiler.lexico.TokenC;
+
+public class MainClass {
+    public static void main(String[] args) {
+        try {
+            // Criação do objeto IsiScanner para ler o arquivo "input.isi"
+        	IsiScanner sc = new IsiScanner("input.jian");
+            TokenC token = null;
+            
+            // Laço para pegar todos os tokens até o final do arquivo
+            do {
+                token = sc.nextToken();  // Obtém o próximo token
+                if (token != null) {
+                    System.out.println(token); // Imprime o token (toString() será chamado automaticamente)
+                }
+            } while (token != null);
+            
+        } catch(LexicalException ex) {
+            // Caso ocorra um erro léxico (ex: erro na análise)
+            System.out.println("Lexical ERROR: " + ex.getMessage());
+            
+        } catch (Exception ex) {
+            // Caso ocorra qualquer outro erro
+            System.out.println("Generic ERROR: " + ex.getMessage());
+        }
+    }
+}
